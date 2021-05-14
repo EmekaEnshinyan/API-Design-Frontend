4c292914729ed79f6c4f68ca8cf506faab162500
Enshinyan <emeka.enshinyan@gmail.com>
2021-05-13 17:47:23 -0700

    http requests

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -1,9 +1,24 @@
+//create functions that can reach backend by making http requests using httpclient
+
 import { Injectable } from '@angular/core';
+import { ClientHttp2Session } from 'http2';
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
+//gets url
+  private apiServerUrl = ''; 
+  //this allows requests to backend
+  constructor(private http: HttpClient) {}
+//gets list of all employees
+//tells http client where to make the requests and type of requests
+  public getEmployees(): Observable<any> {
+    //common js technique to put variables and strings at same time
+    return this.http.get<any>(`${this.apiServerUrl}/employee/all` ) //<-- get requrest, thus no need to pass anything
+    }
+  }
 
-  constructor() { }
-}
+  
